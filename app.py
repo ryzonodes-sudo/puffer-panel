06e45dc109dc00940fc66e2a1f3a83b72553e6b4
ryzonodes-sudo <ryzonodes@gmail.com>
2026-05-24 16:27:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Puffer Panel Running"
+    return "Binder is working!"
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+print("Binder started")
