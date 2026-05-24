407090ae4c9854c32347db8efc1d817a367f2bf5
ryzonodes-sudo <ryzonodes@gmail.com>
2026-05-24 16:46:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return """
-    <h1>PufferPanel Binder Working</h1>
-    <p>Ubuntu 24.04 VPS Ready</p>
-    <p>Docker Installed</p>
-    """
+    return "Ubuntu 24.04 Binder Running"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=7860)
