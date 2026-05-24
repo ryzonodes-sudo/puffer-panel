6b695ca3f9c1a014330e3840808ef46b90c6eb49
ryzonodes-sudo <ryzonodes@gmail.com>
2026-05-24 17:04:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Ubuntu 24 Working"
+    return "Binder Working on Ubuntu 24"
 
-app.run(host="0.0.0.0", port=7860)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=7860)
