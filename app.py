17ee90adcc0fb1411bf6bf2200b0c728a7d4622c
ryzonodes-sudo <ryzonodes@gmail.com>
2026-05-24 16:50:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
@@ -7,4 +8,5 @@ def home():
     return "Ubuntu 24.04 Binder Running"
 
 if __name__ == "__main__":
+    os.system("cat /etc/os-release")
     app.run(host="0.0.0.0", port=7860)
